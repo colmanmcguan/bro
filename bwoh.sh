4c59d858eed9161e0277a3ac39056a5d6c2b1076
Colman McGuan <colman.mcguan@gmail.com>
2022-12-12 20:14:46 -0500

    4 spaces -> 2 spaces

--- bwoh.sh
+++ bwoh.sh
@@ -6,32 +6,32 @@ TB=$(seq -f "tb%g" 1 3)
 TP=$(seq -f "tp%g" 1 12)
 
 if [ $# -ne 0 ]; then
-    for ARG in "$@"; do
-        case $ARG in
-            "b")
-                for CONFIG in $B; do
-                    python3 ./util/bwoh.py $CONFIG
-                done
-                ;;
-            "tb")
-                for CONFIG in $TB; do
-                    python3 ./util/bwoh.py $CONFIG
-                done
-                ;;
-            "tp")
-                for CONFIG in $TP; do
-                    python3 ./util/bwoh.py $CONFIG
-                done
-                ;;
-            *)
-                python3 ./util/bwoh.py $ARG
-                ;;
-        esac
-    done
+  for ARG in "$@"; do
+    case $ARG in
+      "b")
+        for CONFIG in $B; do
+          python3 ./util/bwoh.py $CONFIG
+        done
+        ;;
+      "tb")
+        for CONFIG in $TB; do
+          python3 ./util/bwoh.py $CONFIG
+        done
+        ;;
+      "tp")
+        for CONFIG in $TP; do
+          python3 ./util/bwoh.py $CONFIG
+        done
+        ;;
+      *)
+        python3 ./util/bwoh.py $ARG
+        ;;
+    esac
+  done
 else
-    for CONFIG in $B $TB $TP; do
-        if [ -d $DEFENDED/$CONFIG ]; then
-            python3 ./util/bwoh.py $CONFIG >> bwoh.txt
-        fi
-    done
+  for CONFIG in $B $TB $TP; do
+    if [ -d $DEFENDED/$CONFIG ]; then
+      python3 ./util/bwoh.py $CONFIG >> bwoh.txt
+    fi
+  done
 fi
--- loh.sh
+++ loh.sh
@@ -6,32 +6,32 @@ TB=$(seq -f "tb%g" 1 3)
 TP=$(seq -f "tp%g" 1 12)
 
 if [ $# -ne 0 ]; then
-    for ARG in "$@"; do
-        case $ARG in
-            "b")
-                for CONFIG in $B; do
-                    python3 ./util/loh.py $CONFIG
-                done
-                ;;
-            "tb")
-                for CONFIG in $TB; do
-                    python3 ./util/loh.py $CONFIG
-                done
-                ;;
-            "tp")
-                for CONFIG in $TP; do
-                    python3 ./util/loh.py $CONFIG
-                done
-                ;;
-            *)
-                python3 ./util/loh.py $ARG
-                ;;
-        esac
-    done
+  for ARG in "$@"; do
+    case $ARG in
+      "b")
+        for CONFIG in $B; do
+          python3 ./util/loh.py $CONFIG
+        done
+        ;;
+      "tb")
+        for CONFIG in $TB; do
+          python3 ./util/loh.py $CONFIG
+        done
+        ;;
+      "tp")
+        for CONFIG in $TP; do
+          python3 ./util/loh.py $CONFIG
+        done
+        ;;
+      *)
+        python3 ./util/loh.py $ARG
+        ;;
+    esac
+  done
 else
-    for CONFIG in $B $TB $TP; do
-        if [ -d $DEFENDED/$CONFIG ]; then
-            python3 ./util/loh.py $CONFIG >> loh.txt
-        fi
-    done
+  for CONFIG in $B $TB $TP; do
+    if [ -d $DEFENDED/$CONFIG ]; then
+      python3 ./util/loh.py $CONFIG >> loh.txt
+    fi
+  done
 fi
