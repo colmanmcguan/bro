f92aab5c4c6b383165287d1391cdac27dda4314e
Colman McGuan <colman.mcguan@gmail.com>
2022-12-12 20:35:46 -0500

    rename

--- util/tmstmps.py
+++ util/tmstmps.py
@@ -3,26 +3,15 @@ import sys
 import multiprocessing as mp
 import numpy as np 
 
-def load_trace(file):
+def load_trace(f):
     return np.loadtxt(file, delimiter="\t")
 
 def simulate(f):
     trace = load_trace(f)
-    outgoing = trace[np.where(trace[:,1] == 777)]
-    incoming = trace[np.where(trace[:,1] == -777)]
-    outgoing = outgoing[outgoing[:,0].argsort(kind="mergesort")]
-    incoming = incoming[incoming[:,0].argsort(kind="mergesort")]
-    if len(outgoing) == 0:
-        out_rng = 0
-    else:
-        q75, q25 = np.percentile(outgoing[:,0], [75, 25])
-        out_rng = q75 -q25
-    if len(incoming) == 0:
-        in_rng = 0
-    else:
-        q75, q25 = np.percentile(incoming[:,0], [75, 25])
-        in_rng = q75 -q25
-    return [out_rng, in_rng]
+    trace = trace[trace[:,0].argsort(kind="mergesort")]
+    lastpkt = trace[-1][0]
+    dummy = trace[np.where(abs(trace[:,1]) == 777)]
+    return dummy[:,0]
 
 def parallel(flist, n_jobs=25):
     pool = mp.Pool(n_jobs)
@@ -30,13 +19,18 @@ def parallel(flist, n_jobs=25):
     return arr
 
 if __name__ == '__main__':
+    fdir = os.path.join("defended", sys.argv[1])
     flist  = []
-    for f in os.listdir(sys.argv[1]):
-        flist.append(os.path.join(sys.argv[1], f))
+    for f in os.listdir(fdir):
+        flist.append(os.path.join(fdir, f))
 
     arr = parallel(flist)
     arr = list(zip(*arr))
-    arr = list(arr[0]) + list(arr[1])
-    arr = np.array(arr)
-    print("mean: {}\nstd dev: {}\nmedian: {}".format(arr.mean(), arr.std(), np.median(arr)))
-    print("max: {}".format(arr.max()))
+    narr = []
+    for a in arr:
+        narr = narr + list(a)
+    rr = np.array(narr)
+    print("median timestamp: {}".format(np.median(arr)))
+    print("mean timestamp: {}".format(arr.mean()))
+    print("coef var: {}".format(arr.std() / arr.mean()))
+
